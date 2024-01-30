edc40052ed545d3712370be7001ac21b543c1ea2
kalldrek777 <kalldrek@yandex.ru>
2024-01-30 15:09:32 +0000

    fix models

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -91,9 +91,9 @@ DATABASES = {
     }
 }
 
-# if os.getenv('DATABASE_URL'):
-#     db_from_env = dj_database_url.config(conn_max_age=600)
-#     DATABASES['default'].update(db_from_env)
+if os.getenv('DATABASE_URL'):
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
 
 
 # if DATABASE_TYPE == 'postgreSQL':
