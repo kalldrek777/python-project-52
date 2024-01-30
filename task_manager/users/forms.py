5be2abc43ffa03d47b2f2f757e20ea1681fa64c2
kalldrek777 <kalldrek@yandex.ru>
2024-01-30 15:01:45 +0000

    fix models

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -6,26 +6,26 @@ from .models import CustomUser
 
 class UpdateForm(forms.ModelForm):
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    second_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-input'}))
     username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'second_name', 'username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    second_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-input'}))
     username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'second_name', 'username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
 
 class LoginForm(AuthenticationForm):
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -6,9 +6,12 @@ from django.utils import timezone
 # Create your models here.
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=255, default='')
-    second_name = models.CharField(max_length=255, default='')
+    last_name = models.CharField(max_length=255, default='')
 
     def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    def get_full_name(self):
         return self.first_name + ' ' + self.last_name
 
     def get_author_username(self):
